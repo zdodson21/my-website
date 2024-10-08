231f919ab731d56ca0b43728cac3dcb9dbdcdb2a
Zach Dodson <zdodson21@gmail.com>
2024-10-08 14:48:11 -0400

    new component files

---
+++ src/app-browser.js
@@ -0,0 +1,1 @@
+// Browser app, will display tabs which can be used to display different "websites", which will display slotted content as a website
---
+++ src/window-toolbar.js
@@ -0,0 +1,1 @@
+// Files, Edit, Selection, View, whatever the top bar options need to be.
